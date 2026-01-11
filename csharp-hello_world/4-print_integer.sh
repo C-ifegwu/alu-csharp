0f0b4abf1a5c8ac45dbe73b1f63e17d1b1071cb6
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-11 11:30:36 +0200

    DONE

---
+++ csharp-hello_world/4-print_integer.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# create a new directory 
+mkdir -p  4-print_integer
+
+# Move into the new Directory 
+cd   4-print_integer
+
+# Initialize the new project 
+dotnet new console --force
