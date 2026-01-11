fee25ee492ad1e5468be475f7a84fd7370460744
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-11 08:28:40 +0200

    push

---
+++ csharp-hello_world/3-writeline.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# create a new directory 
+mkdir -p 3-writeline
+
+# Move into the new Directory 
+cd  3-writeline
+
+# Initialize the new project 
+dotnet new console --force
