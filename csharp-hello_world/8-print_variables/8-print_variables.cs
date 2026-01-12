a44efb09ab3d71b355cf8e3e441c197a9dea1ba9
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-12 07:36:00 +0200

    update

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -7,6 +7,6 @@ class Program
         string str1 = "Street";
         string str2 = "Mission";
         int int1 = 972;
-        Console.WriteLine($"{str1} {str2} {int1}");
+        Console.WriteLine($"{int1} {str1} {str2}");
     }
 }
