0a71d00d7b626feaf8e51c91c2320f614999a2e4
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-14 00:52:45 +0200

    Update

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        double percent = .7553;
-        double currency = 98765.4321;
-        Console.WriteLine("Percent: {0:P2} tip", percent);
+        double percent = .2641;
+        double currency = 12345.678;
+        Console.WriteLine("Percent: {0:P2}", percent);
         Console.WriteLine("Currency: {0:C2}", currency);
     }
 }
