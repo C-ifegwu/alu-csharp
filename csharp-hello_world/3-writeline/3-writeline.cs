e48e19146266c142b75e26731d288addc61b0348
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-14 01:55:37 +0200

    Update

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,12 +1,9 @@
-﻿using system;
- 
- class Program
- {
-     static void Main(string[] args)
-     {
-         Console.WriteLine("\"Programming is like building a multilingual puzzle");
-     }
- }
- 
- 
- 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Programming is like building a multilingual puzzle");
+    }
+}
