89657d3baa4ca1b106875865101c7dcf9ae326a4
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-14 00:48:48 +0200

    Update

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-class Program {
-    static void Main(string[] args) {
-        int caseNumber = 1; int caseNumber = 2; int caseNumber = 3; 
-  double percent = 0.00;  double amount = 0.00;
-        // Set values based on the caseNumber
-        if (caseNumber == 1) { percent = -44.98; amount = -9999.99; }
-        if (caseNumber == 2) { percent = 26.41; amount = 12345.68; }
-        if (caseNumber == 3) { percent = 0.00; amount = 0.00; }
-        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = .7553;
+        double currency = 98765.4321;
+
+        Console.WriteLine("Percent: {0:P2} tip", percent);
+        Console.WriteLine("Currency: {0:C2}", currency);
     }
 }
