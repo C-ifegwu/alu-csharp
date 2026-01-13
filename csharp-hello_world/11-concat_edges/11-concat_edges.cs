03ea1961727002998c92be45f324be21b8f79f04
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-14 01:16:15 +0200

    Push

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		str = str.Substring(38, 17) + str.Substring(57, 13) + str.Substring(8, 5);
+		Console.WriteLine(str);
+        }
+}
