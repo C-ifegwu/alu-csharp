37e9200d023afab1b9c5648316183e9360ed2167
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-14 11:51:05 +0200

    Update

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -6,6 +6,7 @@ class Program
 	{
 		string str1 = "Holberton";
 		string str2 = "School";
-		Console.WriteLine("Welcome to " + str1 + " " + str2 + "!");
+		str1 = String.Format("Welcome to {0} {1}!", str1, str2);
+		Console.WriteLine(str1);
 	}
 }
