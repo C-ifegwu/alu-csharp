81ee9714e06e5b6087a66a70ae38853b8a31b5a4
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-16 14:55:41 +0200

    Done

---
+++ csharp-ifelse_loops_methods/3-print_alphab/3-print_alphab.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string output = "";
+
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            if (c != 'e' && c != 'q')
+            {
+                output += c;
+            }
+        }
+
+        Console.Write(output);
+    }
+}
