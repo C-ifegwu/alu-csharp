e7adc9ad64e3607d3f535b198753a578c43d5e5a
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-16 15:49:53 +0200

    Push

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string output = "";
+
+        for (int i = 0; i < 100; i++)
+        {
+            output += i.ToString("D2");
+            if (i != 99)
+                output += ", ";
+        }
+
+        Console.Write(output + "\n");
+    }
+}
