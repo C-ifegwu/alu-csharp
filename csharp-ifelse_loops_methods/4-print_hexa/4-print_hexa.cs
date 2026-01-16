e0a221dcdfeb10f889fc05acfb257e28da52f596
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-16 15:16:02 +0200

    Push

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string result = "";
+
+        for (int i = 0; i <= 98; i++)
+        {
+            result += $"{i} = 0x{i:x}\n";
+        }
+
+        Console.Write(result);
+    }
+}
