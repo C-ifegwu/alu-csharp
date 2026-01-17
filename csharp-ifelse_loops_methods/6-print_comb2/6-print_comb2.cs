eedba6a3b188db9dcf9b1d9592a23dd0b89e7c04
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-17 13:20:10 +0200

    Push

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool first = true;
+
+        for (int i = 0; i <= 8; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                if (!first)
+                    Console.Write(", ");
+
+                Console.Write($"{i}{j}");
+                first = false;
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
