cc267fba65d3cb8ffd47e3e18e4155eec01b6ca5
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-25 20:46:56 +0200

    Push

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[5, 5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i == 2 && j == 2)
+                    matrix[i, j] = 1;
+                else
+                    matrix[i, j] = 0;
+            }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(matrix[i, j]);
+                if (j < 4)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
