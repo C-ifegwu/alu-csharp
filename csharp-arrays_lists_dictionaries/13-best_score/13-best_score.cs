c77750a7eca608adb3765018546654f07b279d0f
C.ifegwu <C.ifegwu@alustudent.com>
2026-01-25 20:38:52 +0200

    Push

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+        {
+            return "None";
+        }
+
+        string bestKey = "None";
+        int maxVal = -1;
+
+        foreach (KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > maxVal)
+            {
+                maxVal = entry.Value;
+                bestKey = entry.Key;
+            }
+        }
+
+        return bestKey;
+    }
+}
